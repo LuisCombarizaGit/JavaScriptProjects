1dab8aff1f60fa6ae3eb166c46f2ce2142650054
Luis Combariza <luis_combariza@outlook.com>
2022-01-17 22:57:02 -0700

    Finished first practice project (N timer)

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -1,11 +1,30 @@
+const daysEI = document.getElementById("days");
+const hoursEI = document.getElementById("hours");
+const minsEI = document.getElementById("mins");
+const secondsEI = document.getElementById("seconds");
+
 const newYears = "1 Jan 2023";
 
+// Function that generates the time in seconds left for year to be over
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  console.log(newYearsDate - currentDate);
+  const totalseconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalseconds / 3600 / 24);
+  const hours = Math.floor(totalseconds / 3600) % 24;
+  const mins = Math.floor(totalseconds / 60) % 60;
+  const seconds = Math.floor(totalseconds) % 60;
+
+  daysEI.innerHTML = days;
+  hoursEI.innerHTML = hours;
+  minsEI.innerHTML = mins;
+  secondsEI.innerHTML = seconds;
+
+  console.log(days, hours, mins, seconds);
 }
 
 // Initial Call
 countdown();
+
+// Callback function to generate new digits
